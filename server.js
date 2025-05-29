8f3ca4e1cb19eb68c68e958192fda342acc32c2f
Kiran Dhungana <dhunganakiran12345@gmail.com>
2025-05-29 22:56:37 +0545

    added comments

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+// to create and handel http server
 const express = require('express');
+// package to handel enviroment + to store secret
 const dotenv = require('dotenv');
+// importing funtion to connect to data base
 const connectDB = require('./config/db');
+
 const authRoutes = require('./routes/authRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const userRoutes =require('./routes/userRoutes');
 
 dotenv.config();
+
+// connect the server to mongoDB
 connectDB();
 
 const app = express();
